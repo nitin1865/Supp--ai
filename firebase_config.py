6168cfc8819d97e4343889f01d67587cded27984
Raaj <nitin@glorymedia.in>
2026-02-19 22:46:40 +0530

    upadted firebase_config & requirements

--- firebase_config.py
+++ firebase_config.py
@@ -1,12 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-SERVICE_ACCOUNT_PATH = os.path.join(BASE_DIR, "firebase_key.json")
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(SERVICE_ACCOUNT_PATH)
+    cred = credentials.Certificate("firebase_key.json")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
